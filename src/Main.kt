6ba4c77e234b2c8cd5073f1dd9c705d7880cec2b
michaelcast01 <michael2003cc@gmail.com>
2025-02-14 14:06:33 -0500

    Operacionesbasicas

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,14 @@
 fun main() {
- println("Hola Mundo")
+    println("Hola Mundo")
     println("Primer ejercicio Kotlin")
     println("Michael Stiven Castillo Castillo")
     println("Escribir poesia")
+
+
+    println("ESTA ES MI RAMA DE DESARROLLO")
+    println("SUMA")
+    println("RESTA")
+    println("MULTIPLICACION")
+    println("DIVISION")
+
+}
